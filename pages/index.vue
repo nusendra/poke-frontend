518a9cf9025ae779bdd171bf9decdd8d437d3604
Nusendra Hanggarawan <admin@nusendra.com>
2021-09-26 14:03:28 +0700

    fix: made a custom data table options

--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,66 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-btn depressed color="primary" @click="logout"> Logout </v-btn>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    :options="options"
+    :server-items-length="totalPokemon"
+    :loading="loading"
+    :footer-props="{
+      'items-per-page-options': [5, 10, 15, 20],
+    }"
+    loading-text="Loading... Please wait"
+    @update:page="changePage"
+    @update:items-per-page="changeRowCount"
+    class="elevation-1"
+  >
+  </v-data-table>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: 'ID',
+          align: 'start',
+          sortable: false,
+          value: 'id',
+        },
+        { text: 'Name', value: 'name' },
+      ],
+      options: {
+        itemsPerPage: 5,
+      },
+      items: [],
+      loading: true,
+      totalPokemon: 0,
+      page: 1,
+    };
+  },
+  async mounted() {
+    await Promise.allSettled([this.getTotalPokemon(), this.fetch()]);
+    this.loading = false;
+  },
   methods: {
+    async getTotalPokemon() {
+      const { data } = await this.$axios.$get(`/pokemon/total`);
+      this.totalPokemon = data;
+    },
+    async fetch() {
+      const { data } = await this.$axios.$get(
+        `/pokemon?page=${this.page}&limit=${this.options.itemsPerPage}`
+      );
+      this.items = data;
+    },
+    changePage(val) {
+      this.page = val;
+      this.fetch();
+    },
+    changeRowCount(val) {
+      this.options.itemsPerPage = val;
+      this.fetch();
+    },
     logout() {
       this.$fire.auth.signOut();
       this.$store.commit('setAuth', null);
